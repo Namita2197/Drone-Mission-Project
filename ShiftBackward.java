e5f5c6b8d89e9e39c2d244782f09a1b5d92d0622
Namita2197 <55005281+Namita2197@users.noreply.github.com>
2019-09-11 23:02:23 -0600

    Delete ShiftBackward.java

--- ShiftBackward.java
+++
@@ -1,9 +0,0 @@
-public class ShiftBackward extends Instruction {
-    public ShiftBackward(Communicator communicator) {
-        super(communicator);
-    }
-    public void executeInstruction() throws  Exception{
-        this.communicator.sendSignal("back 25");
-        Thread.sleep(2000);
-    }
-}
